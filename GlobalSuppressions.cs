9f613f8116dbbfd78e58105043424da7f86cde5e
VicVal <104785516+V1cVal@users.noreply.github.com>
2022-06-11 18:10:54 +0300

    Delete GlobalSuppressions.cs

--- GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-//using System.Diagnostics.CodeAnalysis;
-
-//[assembly: SuppressMessage("Style", "IDE0059:Ненужное присваивание значения", Justification = "<Ожидание>", Scope = "member", Target = "~M:Sudoku.GameLogic.SwapBlocksInRow(System.Int32[,],System.Int32)")]
-//[assembly: SuppressMessage("Style", "IDE0059:Ненужное присваивание значения", Justification = "<Ожидание>", Scope = "member", Target = "~M:Sudoku.GameLogic.TranspositionMatrix(System.Int32[,],System.Int32)")]
