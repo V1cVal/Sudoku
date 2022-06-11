e398015588d0c9ce5bcb17842d35844bb67612a0
VicVal <104785516+V1cVal@users.noreply.github.com>
2022-06-11 18:10:37 +0300

    Add files via upload

---
+++ GameLogic.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+
+    public class GameLogic {
+
+        public void GenerateMatrix(int[,] matrix, int k) {
+
+            for (int i = 0; i < (k * k); i++) {
+                for (int j = 0; j < (k * k); j++) {
+                    matrix[i, j] = ((i * k) + (i / k) + j) % (k * k) + 1;
+                }
+            }
+        }
+
+        public void TranspositionMatrix(int[,] matrix, int k) {
+
+            int[,] tMatrix = new int[k * k, k * k];
+
+            for (int i = 0; i < (k * k); i++) {
+                for (int j = 0; j < (k * k); j++) {
+                    tMatrix[i, j] = matrix[j, i];
+                }
+            }
+
+			for (int i = 0; i < (k * k); i++) {
+				for (int j = 0; j < (k * k); j++) {
+					matrix[i, j] = tMatrix[i, j];
+				}
+			}
+        }
+
+        public void SwapRowsInBlock(int[,] matrix, int k) {
+
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+
+            var block = r.Next(0, k);
+            var row1 = r.Next(0, k);
+            var row2 = r.Next(0, k);
+            while (row1 == row2) {
+                row2 = r.Next(0, k);
+            }
+
+            var line1 = block * k + row1;
+            var line2 = block * k + row2;
+            
+            for (int i = 0; i < k * k; i++) {
+                var tmp = matrix[line1, i];
+                matrix[line1, i] = matrix[line2, i];
+                matrix[line2, i] = tmp;
+			}
+        }
+
+        public void SwapColumnsInBlock(int[,] matrix, int k) {
+
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+
+            var block = r.Next(0, k);
+            var row1 = r.Next(0, k);
+            var row2 = r.Next(0, k);
+            while (row1 == row2) {
+                row2 = r.Next(0, k);
+            }
+
+            var line1 = block * k + row1;
+            var line2 = block * k + row2;
+
+            for (int i = 0; i < k * k; i++) {
+                var tmp = matrix[i, line1];
+                matrix[i, line1] = matrix[i, line2];
+                matrix[i, line2] = tmp;
+            }
+        }
+
+        public void SwapBlocksInRow(int[,] matrix, int k) {
+
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+
+            var block1 = r.Next(0, k);
+            var block2 = r.Next(0, k);
+            
+            while (block1 == block2) {
+                block2 = r.Next(0, k);
+            }
+
+            block1 *= k;            
+            block2 *= k;            
+
+            for (int i = 0; i < k * k; i++) {
+                var blockTmp = block2;
+                for (int j = block1; j < block1 + k; j++) {
+                    var tmp = matrix[j, i];
+                    matrix[j, i] = matrix[blockTmp, i];
+                    matrix[blockTmp, i] = tmp;
+					blockTmp++;
+				}
+			}
+        }
+
+        public void SwapBlocksInColumn(int[,] matrix, int k) {
+
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+
+            var block1 = r.Next(0, k);
+            var block2 = r.Next(0, k);
+
+            while (block1 == block2) {
+                block2 = r.Next(0, k);
+            }
+
+            block1 *= k;            
+            block2 *= k;            
+
+            for (int i = 0; i < k * k; i++) {
+                var blockTmp = block2;
+                for (int j = block1; j < block1 + k; j++) {
+                    var tmp = matrix[i, j];
+                    matrix[i, j] = matrix[i, blockTmp];
+                    matrix[i, blockTmp] = tmp;
+                    blockTmp++;
+                }
+            }          
+        }
+
+        public void RefreshMatrixSwitch(int i, int[,] matrix, int k) {
+
+			switch (i) {
+
+                case 0:
+                    TranspositionMatrix(matrix, k);
+                    break;
+
+                case 1:
+                    SwapRowsInBlock(matrix, k);
+                    break;
+
+                case 2:
+                    SwapColumnsInBlock(matrix, k);
+                    break;
+
+                case 3:
+                    SwapBlocksInRow(matrix, k);
+                    break;
+
+                case 4:
+                    SwapBlocksInColumn(matrix, k);
+                    break;
+
+                default:
+                    TranspositionMatrix(matrix, k);
+                    break;
+            }
+		}
+
+        public void RefreshMatrix(int[,] matrix, int k) {
+
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+
+            for (int i = 0; i < 30; i++) {
+                RefreshMatrixSwitch(r.Next(0,5), matrix, k);
+			}
+        }
+
+        public void PressedButton(object sender, EventArgs e) {
+
+            Button pressedButton = sender as Button;
+            string buttonText = pressedButton.Text;
+
+            if (string.IsNullOrEmpty(buttonText)) {
+                pressedButton.Text = "1";
+			}
+            else {
+                int num = int.Parse(buttonText);
+                num++;
+
+                if(num == 10) {
+                    num = 1;
+				}
+                pressedButton.Text = num.ToString();
+			}
+		}
+        
+        public void HideButtonsSwitch(int[,] matrix, int k, int key) {
+
+            switch(key) {
+                case 0:
+                    HideButtons(matrix, k, 30);
+                    break;
+
+                case 1:
+                    HideButtons(matrix, k, 40);
+                    break;
+
+                case 2:
+                    HideButtons(matrix, k, 50);
+                    break;
+                case 3:
+                    HideButtons(matrix, k, 5);
+                    break;
+                default:
+                    HideButtons(matrix, k, 30);
+                    break;
+            }
+		}
+
+        public void HideButtons(int[,] matrix, int k, int difficultValue) {
+
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+            int difficultValueTmp = difficultValue;
+
+            while (difficultValueTmp > 0) {
+                for (int i = 0; i < (k * k); i++) {
+                    for (int j = 0; j < (k * k); j++) {
+
+                        int aNull = r.Next(0,3);
+
+                        if (aNull == 0) {
+                            matrix[i, j] = 0;
+                            difficultValueTmp--;
+                        }
+
+						if (difficultValueTmp == 0) {
+                            break;
+						}
+                    }
+
+                    if (difficultValueTmp == 0) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public int KeyObtaining(string difficult) {
+
+            switch(difficult) {
+                case "Средний":
+                    return 0;
+
+                case "Сложный":
+                    return 1;
+
+                case "Профессионал":
+                    return 2;
+
+                case "Тренировочный":
+                    return 3;
+
+                default:
+                    return 0;
+            }
+		}
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Sudoku
+{
+    public partial class MainForm : Form
+    {
+
+        private const int k = 3;
+        public int[,] matrix = new int[k * k, k * k];
+        public int[,] matrixForChange = new int[k * k, k * k];
+
+        GameLogic gameLogic = new GameLogic();
+
+        public MainForm() {
+
+            InitializeComponent();
+            InitializeMatrix();
+
+            string filename = "C:\\Users\\ilinv\\Desktop\\Курсовая по ООП\\Sudoku\\Для файла с правилами\\SudokuRules.txt";
+            String line;
+
+            try {
+                System.IO.StreamReader sr = new System.IO.StreamReader(filename);
+                line = sr.ReadLine();
+
+                while (line != null) {
+                    textBox1.Text += line;
+                    textBox1.Text += Environment.NewLine;
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e) {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+            gameLogic.GenerateMatrix(matrix, k);
+            gameLogic.RefreshMatrix(matrix, k);
+
+            CreateMatrix(); 
+        }
+
+        private void CreateMatrix() {
+            int tmp = 1;
+            for (int i = 1; i < (k * k) + 1; i++) {
+                for (int j = 1; j < (k * k) + 1; j++) {
+                    panel1.Controls["button" + tmp].Text = matrix[i - 1, j - 1].ToString();
+                    panel1.Controls["button" + tmp].Enabled = false;
+                    panel1.Controls["button" + tmp].BackColor = Color.White;
+                    tmp++;
+                }
+            }
+        }
+        
+		private void NewGameButton_Click(object sender, EventArgs e) {
+
+            gameLogic.GenerateMatrix(matrix, k);           
+            gameLogic.RefreshMatrix(matrix, k);
+            CreateMatrix();
+
+            for (int i = 1; i < (k * k) + 1; i++) {
+                for (int j = 1; j < (k * k) + 1; j++) {
+                     matrixForChange[i - 1, j - 1] = matrix[i - 1, j - 1]; 
+                }
+            }
+
+            if (comboBox1.SelectedItem != null) {
+                gameLogic.HideButtonsSwitch(matrixForChange, k, gameLogic.KeyObtaining(comboBox1.SelectedItem.ToString()));                   
+            }
+            else {
+                gameLogic.HideButtonsSwitch(matrixForChange, k, 0);
+            }
+
+            int tmp = 1;
+            for (int i = 1; i < (k * k) + 1; i++) {
+                for (int j = 1; j < (k * k) + 1; j++) {
+                   if (matrixForChange[i - 1, j - 1] == 0) {
+                        panel1.Controls["button" + tmp].Enabled = true;
+                        panel1.Controls["button" + tmp].Text = ""; 
+                    }
+                    tmp++;
+                }
+            }
+        }
+
+        private void InitializeMatrix() {
+            int tmp_G = 1;
+            for (int i = 1; i < (k * k) + 1; i++) {
+                for (int j = 1; j < (k * k) + 1; j++) {
+                    panel1.Controls["button" + tmp_G].Click += gameLogic.PressedButton;
+                    tmp_G++;
+                }
+            }
+        }
+
+		private void WinnerCheckButton_Click(object sender, EventArgs e) {
+
+            int tmp_G = 1;
+            for (int i = 1; i < (k * k) + 1; i++) {
+                for (int j = 1; j < (k * k) + 1; j++) {
+                    if (panel1.Controls["button" + tmp_G].Text != "") {
+                        matrixForChange[i - 1, j - 1] = int.Parse(panel1.Controls["button" + tmp_G].Text);
+                        tmp_G++;
+                    }
+                    else {
+                        matrixForChange[i - 1, j - 1] = 0;
+                        tmp_G++;
+                    }  
+                }
+            }
+
+            int tmp_G2 = 1;
+            int countTmp = 0;
+
+            for (int i = 1; i < (k * k) + 1; i++) {
+                for (int j = 1; j < (k * k) + 1; j++) {
+
+                    panel1.Controls["button" + tmp_G2].Enabled = false;
+
+                    if (matrix[i - 1, j - 1] != matrixForChange[i - 1, j - 1]) {
+                        panel1.Controls["button" + tmp_G2].Enabled = true;
+                        panel1.Controls["button" + tmp_G2].BackColor = Color.Coral;
+                        countTmp++;
+                    }
+                    tmp_G2++;
+                }
+            }
+
+            if (countTmp != 0) {
+                MessageBox.Show("Судоку решено неверно, но вы можете исправить ошибки");
+            }
+			else {
+                MessageBox.Show("Поздравляю! Судоку решено верно");
+            }
+        }
+	}
+}
